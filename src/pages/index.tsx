4035edd34bdeb56a37003a3deaa6e54c06b8d9b4
Polyana Balotin <polybalotin@hotmail.com>
2022-06-17 17:59:57 -0300

    create Dialog

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import type { NextPage } from 'next'
-import Title from '../userInterface/components/Title/Title'
+import type { NextPage } from 'next';
+import Title from '../userInterface/components/Title/Title';
 import List from '../userInterface/components/List/List';
+import { Dialog, TextField, Grid, DialogActions, Button } from '@mui/material';
 
 
 const Home: NextPage = () => {
@@ -25,6 +26,41 @@ const Home: NextPage = () => {
         }
       ]}
       />
+
+      <Dialog
+        open={ false }
+        fullWidth
+        PaperProps={{ sx: { padding: 5 } }}
+      >
+        <Grid container spacing={ 2 } >
+          <Grid item xs={ 12 } >
+            <TextField
+              type={ 'email' }
+              label={ 'E-mail' }
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={ 12 } >
+            <TextField
+              type={ 'number' }
+              label={ 'Quantia por mês' }
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+        <DialogActions sx={{ marginTop: 5 }}>
+          <Button
+            color={ 'secondary' }
+          >
+            Cancelar
+          </Button>
+          <Button
+            variant={ 'contained' }
+          >
+            Confirmar adoção
+          </Button>
+        </DialogActions>
+      </Dialog>
 
     </div>
   )
