be40fa6465dbb8e80af976bcbe6a57fa4a260636
Polyana Balotin <polybalotin@hotmail.com>
2022-06-17 21:40:31 -0300

    get data from backend

--- src/data/hooks/pages/useIndex.ts
+++ src/data/hooks/pages/useIndex.ts
@@ -1,23 +1,50 @@
-import { useState } from "react"
-import { Pet } from "../../@types/Pet"
+import { useState, useEffect } from 'react';
+import { Pet } from '../../@types/Pet';
+import { ApiService } from '../../services/ApiService';
+import { AxiosError } from 'axios';
 
 export function useIndex() {
-  const [petsList, setPetsList] = useState(
-    [
-      {
-        id: 1,
-        name: 'Bidu',
-        history: 'sfsefesfs',
-        image: 'https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info'
-      }
-    ]
-  ),
+  const [petsList, setPetsList] = useState<Pet[]>([]),
   [selectedPet, setSelectedPet] = useState<Pet | null>(null),
   [email, setEmail] = useState(''),
   [value, setValue] = useState(''),
   [message, setMessage] = useState('');
 
-    function adopt(){}
+  useEffect(() => {
+    ApiService.get('/pets')
+    .then((response) => {
+      setPetsList(response.data);
+    })
+  }, [])
+
+  function validateAdoptionData() {
+    return email.length > 0 && value.length > 0;
+  }
+
+  function clearForm() {
+    setEmail('');
+    setValue('');
+  }
+
+  function adopt(){
+    if (selectedPet !== null) {
+      if (validateAdoptionData()) {
+        ApiService.post('/adoptions', {
+          pet_id: selectedPet.id,
+          email,
+          value,
+        }).then(() => {
+          setSelectedPet(null),
+          setMessage('Pet adotado com sucesso!')
+          clearForm();
+        }).catch((error: AxiosError) => {
+          setMessage(error.response?.data.message)
+        })
+      } else {
+        setMessage('Preencha todos os campos corretamente')
+      }
+    }
+  }
 
   return{
     petsList,
