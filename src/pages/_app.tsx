ecc11da59a4a8c799c16805bc2477d8ab98fbf84
Polyana Balotin <polybalotin@hotmail.com>
2022-06-19 16:34:02 -0300

    create admin header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,14 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material';
 import theme from '../userInterface/themes/theme';
 import Header from '../userInterface/components/Header/Header';
+import AdminHeader from '../userInterface/components/AdminHeader/AdminHeader';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return(
     <ThemeProvider theme={ theme }>
-      <Header />
+      { router.pathname === '' ? <Header /> : <AdminHeader /> }
       <Component { ...pageProps } />
     </ThemeProvider>
   )
